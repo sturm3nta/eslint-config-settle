8cbaa4bd01ad08a8a9576153467545607df39525
sturmenta <sturmenta@gmail.com>
2020-01-30 11:37:26 -0300

    [ZTL-780] - split in node/react

---
+++ base.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  settings: { 'import/ignore': ['src/types/index.js'] },
+  rules: {
+    '-------- ↓↓↓ DISABLED ↓↓↓ -----------': '------------------------------',
+    'no-param-reassign': 0,
+    'consistent-return': 0,
+    'no-nested-ternary': 0,
+    'no-use-before-define': 0,
+    'no-underscore-dangle': 0,
+    'class-methods-use-this': 0,
+    'import/prefer-default-export': 0,
+    'import/no-named-as-default-member': 0,
+
+    '-------- ↓↓↓ WARNING ↓↓↓ -----------': '------------------------------',
+    eqeqeq: 1,
+    'no-shadow': 1,
+    'no-plusplus': 1,
+    'import/no-cycle': 1,
+    'no-return-assign': 1,
+    'import/no-extraneous-dependencies': 1,
+
+    '-------- ↓↓↓ ERROR ↓↓↓ -----------': '------------------------------',
+    camelcase: 0,
+    'no-unused-vars': 2
+  }
+};
---
+++ react.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ['prettier/react', './base'],
+  env: { browser: true },
+  parser: 'babel-eslint',
+  settings: { 'import/ignore': ['src/types/index.js'] },
+  rules: {
+    '-------- ↓↓↓ DISABLED ↓↓↓ -----------': '------------------------------',
+    'jsx-a11y/alt-text': 0,
+    'react/prop-types': [0],
+    'jsx-a11y/anchor-is-valid': 0,
+    'react/destructuring-assignment': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+
+    '-------- ↓↓↓ WARNING ↓↓↓ -----------': '------------------------------',
+    'react/button-has-type': 1,
+    'react/jsx-boolean-value': 1,
+    'react/no-array-index-key': 1,
+    'react/require-default-props': 1,
+    'react/prefer-stateless-function': 1,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+
+    '-------- ↓↓↓ ERROR ↓↓↓ -----------': '------------------------------',
+    'react/no-access-state-in-setstate': 2
+  }
+};
